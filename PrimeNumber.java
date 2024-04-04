dd4a0c81e8014bfa4e497707d26a0c9fbcc41324
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2024-04-04 13:12:03 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+class PrimeNumber{
+  public static void main(String[] args) {
+  	int num=23;
+    System.out.println(isPrime(num));
+  }
+  public static boolean isPrime(int num){
+  	if(num<=1)
+    	return false;
+    for(int i=2;i<=Math.sqrt(num);i++){
+    	if(num%i==0)
+        	return false;
+       }
+       return true;
+  }
+}
