f71423982b9694ba0a2a2e88b5073a1a5157e75e
mdabdussahid <mdabdussahid01@gmail.com>
2025-08-13 17:33:02 +0530

    feat: add sorting array

---
+++ JAVASCRIPT/SortArray.js
@@ -0,0 +1,13 @@
+let arr = [1, 5, 7, 2, 3];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+
+console.log(arr); 
