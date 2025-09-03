d8b4acdf9f0ff7523de348cf71aaf8ee2c55b707
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2025-09-03 22:50:28 +0530

    remove conflict

--- JAVASCRIPT/SortArray.js
+++ JAVASCRIPT/SortArray.js
@@ -1,15 +1,14 @@
 const array = [1, 5, 7, 2, 3];
-
-const sort=(arr)=>{
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr.length-1;j++){
-            if(arr[j]>arr[j+1]){ 
-                let temp=arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp
-            }
-        }
+const sort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
-    return arr;
-}
-console.log(sort(array))
+  }
+  return arr;
+};
+console.log(sort(array));
