21a47d6e23b7bfa7741e4925f22de38fc45608e3
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2026-01-28 15:00:43 +0530

    Delete SetTimeoutUsingVar.js

--- JAVASCRIPT/SetTimeoutUsingVar.js
+++
@@ -1,7 +0,0 @@
-for (var i = 0; i < 3; i++) {
-  (function(j) { // Create a new function scope
-    setTimeout(function() {
-      console.log(j); // Use the captured argument 'j'
-    }, 1000);
-  })(i); // Pass the current value of 'i' immediately
-}
