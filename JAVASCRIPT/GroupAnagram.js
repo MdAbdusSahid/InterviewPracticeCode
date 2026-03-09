281d37082a9acc0202ea4ea8df1fb1db474a13bf
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2026-03-09 21:33:16 +0530

    Create GroupAnagram.js

---
+++ JAVASCRIPT/GroupAnagram.js
@@ -0,0 +1,17 @@
+function groupAnagrams(words) {
+  const map = {};
+
+  for (let word of words) {
+    const key = word.split('').sort().join('');
+
+    if (!map[key]) {
+      map[key] = [];
+    }
+
+    map[key].push(word);
+  }
+
+  return Object.values(map);
+}
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
