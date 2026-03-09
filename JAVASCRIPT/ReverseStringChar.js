eb5216357b808bc4f4f08bd35a204a4359907dc2
MD ABDUS SAHID <47377425+MdAbdusSahid@users.noreply.github.com>
2026-03-09 23:51:22 +0530

    Update ReverseStringChar.js

--- JAVASCRIPT/ReverseStringChar.js
+++ JAVASCRIPT/ReverseStringChar.js
@@ -6,3 +6,14 @@
     return newStr;
   };
   console.log(reverseStringChar("Hello World"));
+  const reverseStringChar = (str) => {
+    str = str.split("");
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+      [str[left], str[right]] = [str[right], str[left]];
+      left++;
+      right--;
+    }
+    return str.join("");
+  };
